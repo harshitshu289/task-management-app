177bc2fc013e7e46709a68e5320600726a9d2705
Harshit Shukla <harshitshukla289@gmail.com>
2024-12-12 19:26:26 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create Axios instance with default settings
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
+  baseURL: "https://task-management-app-backend-r097.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
